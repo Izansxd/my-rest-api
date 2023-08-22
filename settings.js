7fdcebf5a1bd120921dc4f11588447a57db7a432
Izansxd <115550578+Izansxd@users.noreply.github.com>
2023-08-22 12:19:57 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'Izansxd'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "fazahengkerwibutzy1dunia@gmail.com" //email
+global.email_password = "phbegbjccvbfilek" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
